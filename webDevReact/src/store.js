fb74ca181c8250532b1f2c8fc77f887f9d12e21f
Belen Castellanos Frederick <castellanosbelen3@gmail.com>
2026-07-21 11:40:56 -0500

    api url fixes

--- webDevReact/src/store.js
+++ webDevReact/src/store.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 
 const PORT = process.env.PORT || 8080;
-const API_URL = import.meta.env.VITEAPI_URL || 'http://localhost8080';
+const API_URL = import.meta.env.VITE_API_URL || `http://localhost:${PORT}`;
 
 //boiler plate 
 const useStore = create((set, getStore) => ({
